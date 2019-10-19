859c90bad0f40ec3f06c0e9d5e4bdce393a27024
clarice <clarity.clarice@gmail.com>
2019-10-18 21:55:45 -0300

    Configurando funcionalidade de emails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '736fe18302268e',
+        pass: '5d0e0847a897b8'
+    },
+    default: {
+        from: 'Equipe GymPoint <clarity.clarice@gmail.com>'
+    }
+}
