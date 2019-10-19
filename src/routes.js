7e139fff57157b91d8b7eed0329d9624b22605aa
clarice <clarity.clarice@gmail.com>
2019-10-18 21:56:37 -0300

    Inclusão ode novas rotas

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import StudentController from './app/controllers/StudentController'
 import PlanController from './app/controllers/PlanController'
 import FileController from './app/controllers/FileController'
 import EnrollmentController from './app/controllers/EnrollmentController'
+import NotificationController from './app/controllers/NotificationController'
 
 import authMiddleware from './app/middlewares/auth'
 
@@ -25,15 +26,21 @@ routes.use(authMiddleware) // Only authorizes next ones if authenticated
 routes.post('/plans', PlanController.store)
 routes.post('/students', StudentController.store)
 routes.post('/files', upload.single('file'), FileController.store)
-routes.post('/enrollment', EnrollmentController.store)
+routes.post('/enrollments', EnrollmentController.store)
 
 //Routes from reading (get)
 routes.get('/users', UserController.index)
-
+routes.get('/students', StudentController.index)
+routes.get('/plans', PlanController.index)
+routes.get('/enrollments', EnrollmentController.index)
 
 //Routes for updating (put)
 routes.put('/students', StudentController.update)
 routes.put('/users', UserController.update)
 routes.put('/plans', PlanController.update)
+routes.put('/notifications/:id', NotificationController.update)
+
+//Routes for deleting (delete)
+routes.delete('/enrollments/:id', EnrollmentController.delete)
 
 export default routes
